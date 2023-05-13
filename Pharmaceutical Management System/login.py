663af93ef269f7276c2e69a80ebf3f26060cffbf
mariam-diab <mariam.diab@ejust.edu.eg>
2023-05-13 18:46:02 +0300

    Update commit

--- Pharmaceutical Management System/login.py
+++ Pharmaceutical Management System/login.py
@@ -1,11 +1,10 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QDialog, QMessageBox
-from PyQt5 import uic, QtWidgets
 from login_ui import Ui_Dialog
 import mydatabase
 import signup
 import start
-from main import widget
+
 import user_data
 
 from user_data import name
@@ -22,8 +21,9 @@ class LoginDialog(QDialog, Ui_Dialog):
 
     def open_start(self):
         self.start_dialog = start.StartDialog()
-        widget.addWidget(self.start_dialog)
-        widget.setCurrentIndex(widget.currentIndex()+1)
+        self.start_dialog.show()
+        self.close()
+
 
     def login(self):
         username = self.username_lineEdit.text()
@@ -40,22 +40,15 @@ class LoginDialog(QDialog, Ui_Dialog):
         else:
             QMessageBox.warning(self, "Login Error", "Invalid username or password")
         mydatabase.db.close()
-        self.close()
 
     def open_signup(self):
         self.signup_dialog = signup.SignupDialog()
-        widget.addWidget(self.signup_dialog)
-        widget.setCurrentIndex(widget.currentIndex()+1)
+        self.signup_dialog.show()
+        self.close()
 
-#
-# if __name__ == '__main__':
-#     app = QApplication(sys.argv)
-#     widget = QtWidgets.QStackedWidget()
-#     dialog = LoginDialog()
-#     dialog2 = signup.SignupDialog()
-#     widget.addWidget(dialog)
-#     widget.addWidget(dialog2)
-#     widget.setFixedSize(700, 500)
-#     widget.show()
-#     #dialog.show()
-#     sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    dialog = LoginDialog()
+    dialog.show()
+    sys.exit(app.exec_())
