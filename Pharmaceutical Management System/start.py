a6fffe12090d339e0766f41f2b9c16f52f378645
mariam-diab <mariam.diab@ejust.edu.eg>
2023-05-14 01:01:47 +0300

    Update commit

--- Pharmaceutical Management System/start.py
+++ Pharmaceutical Management System/start.py
@@ -13,8 +13,8 @@ class StartDialog(QDialog, Ui_Dialog):
         super().__init__()
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
-        self.ui.homeName.setText(user_data.name)
-        self.ui.homeName.setStyleSheet("color: white;border-radius: 20px; border-color:rgb(0, 0, 0); background-image: url(:/newPrefix/start2.jpg); font-size: 28pt; text-align:center;")
+        self.ui.homeName_2.setText(user_data.name)
+        self.ui.homeName_2.setStyleSheet("color: white;border-radius: 20px; border-color:rgb(0, 0, 0); background-image: url(:/newPrefix/start2.jpg); font-size: 28pt; text-align:center;")
         self.welcomeScreen = None
         self.ui.Signout.clicked.connect(self.signOut)
 
@@ -34,9 +34,9 @@ class StartDialog(QDialog, Ui_Dialog):
 
 
 
-# if __name__ == "__main__":
-#     app = QApplication(sys.argv)
-#     dialog = StartDialog()
-#     dialog.show()
-#     sys.exit(app.exec_())
-#
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    dialog = StartDialog()
+    dialog.show()
+    sys.exit(app.exec_())
+
