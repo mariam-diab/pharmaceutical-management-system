e797cf216f045aab4c316584cc53a495b970e71c
toqaalaa20 <90696437+toqaalaa20@users.noreply.github.com>
2023-05-13 10:50:32 +0300

    Linked Start

---
+++ Pharmaceutical Management System/start.py
@@ -0,0 +1,20 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QDialog
+from start_ui import Ui_Dialog
+import signup
+import login
+
+
+class StartDialog(QDialog, Ui_Dialog):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    dialog = StartDialog()
+    dialog.show()
+    sys.exit(app.exec_())
+
