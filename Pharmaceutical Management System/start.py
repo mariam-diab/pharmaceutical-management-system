6834d286dce9af859683ca3d4a745dee86694d4c
mariam-diab <mariam.diab@ejust.edu.eg>
2023-05-14 16:52:13 +0300

    Update commit

--- Pharmaceutical Management System/start.py
+++ Pharmaceutical Management System/start.py
@@ -23,18 +23,41 @@ class StartDialog(QDialog, Ui_Dialog):
         self.ui.Signout.clicked.connect(self.signOut)
         self.ui.signup_button_8.clicked.connect(self.addToStock)
         self.ui.signup_button_5.clicked.connect(self.print)
-        self.ui.signup_button_4.clicked.connect(self.printorder)
+        self.ui.ptintOrder.clicked.connect(self.printorder)
         self.load_data()
         self.ui.signup_button_6.clicked.connect(self.search_drug)
-        self.ui.signup_button_7.clicked.connect(self.add_drug)
+        self.ui.addbtn.clicked.connect(self.add_order)
+        self.total = 0
+        self.ui.totalOrders.setText(str(self.total))
 
-
-    def add_drug(self):
-        customer_name = self.ordersName.text()
-        mobile_number = self.ordersMobile.text()
-        drug_name = self.startShift_3.text()
+    def add_order(self):
+        customer_name = self.ui.ordersName.text()
+        customer_phone = self.ui.ordersMobile.text()
+        drug_name = self.ui.orderName.text()
+        quantity = int(self.ui.quantity.text())
         current_date = datetime.date.today()
         current_time = datetime.datetime.now().time()
+        check_query = "SELECT quantity FROM stock WHERE item_name = %s"
+        mydatabase.mycursor.execute(check_query, (drug_name,))
+        stock_data = mydatabase.mycursor.fetchone()
+        if not stock_data:
+            QMessageBox.critical(self, "Error", f"{drug_name} is not available in stock.")
+        elif stock_data[0] < quantity:
+            QMessageBox.critical(self, "Error", f"No enough quantity in stock for {drug_name}. Available stock: {stock_data[0]}.")
+        else:
+            price_query = "SELECT price FROM stock WHERE item_name = %s"
+            mydatabase.mycursor.execute(price_query, (drug_name,))
+            price_data = mydatabase.mycursor.fetchone()
+            price = price_data[0]*quantity
+            self.total += price
+            self.ui.totalOrders.setText(str(self.total))
+            query = "INSERT INTO purchases (item_name, quantity, purchase_date, purchase_time, customer_name, customer_phone, price) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+            values = (drug_name, quantity, current_date, current_time, customer_name, customer_phone, price)
+            mydatabase.mycursor.execute(query, values)
+            update_query = "UPDATE stock SET quantity = quantity - %s WHERE item_name = %s"
+            mydatabase.mycursor.execute(update_query, (quantity, drug_name))
+            mydatabase.db.commit()
+            QMessageBox.information(self, "Success", "Drug added successfully.")
 
 
     def search_drug(self):
@@ -76,8 +99,11 @@ class StartDialog(QDialog, Ui_Dialog):
     def print(self):
         QMessageBox.information(self, "print", "Printing.....")
 
+
     def printorder(self):
         QMessageBox.information(self, "print", "Printing.....")
+        self.total = 0
+        self.ui.totalOrders.setText(str(self.total))
 
 
 # if __name__ == "__main__":
